9a4f09017515602643af0ffe2142d161d48e8aee
justastam <47006709+JustasTamulis@users.noreply.github.com>
2020-02-11 05:21:33 +0000

    Add files via upload

--- neural_test3.py
+++ neural_test3.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import numpy as np
-
+import cv2
 import glob
 import imageio
 import matplotlib.pyplot as plt
@@ -15,29 +15,29 @@ from tensorflow.keras import layers
 import time
 from IPython import display
 
-tf.enable_eager_execution()
+tf.compat.v1.enable_eager_execution()
 
 
 def make_generator_model():
     model = tf.keras.Sequential()
-    model.add(layers.Dense(7*7*256, use_bias=False, input_shape=(100,)))
+    model.add(layers.Dense(3*5*3*5*8*3, use_bias=False, input_shape=(100,)))
     model.add(layers.BatchNormalization())
     model.add(layers.LeakyReLU())
 
-    model.add(layers.Reshape((7, 7, 256)))
-    assert model.output_shape == (None, 7, 7, 256) # Note: None is the batch size
+    model.add(layers.Reshape((45, 60, 2)))
+    # assert model.output_shape == (None, 45, 60, 8) # Note: None is the batch size
 
-    model.add(layers.Conv2DTranspose(128, (5, 5), strides=(1, 1), padding='same', use_bias=False))
-    assert model.output_shape == (None, 7, 7, 128)
+    model.add(layers.Conv2DTranspose(32, (10, 10), strides=(2, 2), padding='same', use_bias=False))
+    # assert model.output_shape == (None, 7, 7, 128)
     model.add(layers.BatchNormalization())
     model.add(layers.LeakyReLU())
 
-    model.add(layers.Conv2DTranspose(64, (5, 5), strides=(2, 2), padding='same', use_bias=False))
-    assert model.output_shape == (None, 14, 14, 64)
+    model.add(layers.Conv2DTranspose(16, (5, 5), strides=(2, 2), padding='same', use_bias=False))
+    # assert model.output_shape == (None, 14, 14, 64)
     model.add(layers.BatchNormalization())
     model.add(layers.LeakyReLU())
 
-    model.add(layers.Conv2DTranspose(1, (5, 5), strides=(2, 2), padding='same', use_bias=False, activation='tanh'))
+    model.add(layers.Conv2DTranspose(3, (5, 5), strides=(2, 2), padding='same', use_bias=False, activation='tanh'))
     # assert model.output_shape == (None, 28, 28, 1)
 
     return model
@@ -45,12 +45,12 @@ def make_generator_model():
 
 def make_discriminator_model():
     model = tf.keras.Sequential()
-    model.add(layers.Conv2D(64, (5, 5), strides=(2, 2), padding='same',
-                                     input_shape=[28, 28, 1]))
+    model.add(layers.Conv2D(28, (10, 10), strides=(4, 4), padding='same',
+                                     input_shape=[360, 480, 3]))
     model.add(layers.LeakyReLU())
     model.add(layers.Dropout(0.3))
 
-    model.add(layers.Conv2D(128, (5, 5), strides=(2, 2), padding='same'))
+    model.add(layers.Conv2D(64, (5, 5), strides=(8, 8), padding='same'))
     model.add(layers.LeakyReLU())
     model.add(layers.Dropout(0.3))
 
@@ -90,7 +90,6 @@ def train_step(images):
     generator_optimizer.apply_gradients(zip(gradients_of_generator, generator.trainable_variables))
     discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, discriminator.trainable_variables))
 
-
 def train(dataset, epochs):
     for epoch in range(epochs):
         start = time.time()
@@ -124,35 +123,67 @@ def generate_and_save_images(model, epoch, test_input):
       plt.imshow(predictions[i, :, :, 0] * 127.5 + 127.5, cmap='gray')
       plt.axis('off')
 
-  plt.savefig('image_at_epoch_{:04d}.png'.format(epoch))
-  plt.show()
+  plt.savefig('image_{:04d}.png'.format(epoch))
+
+def getVideo(name):
+    cap = cv2.VideoCapture(name)
+    frameCount = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    frameWidth = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    frameHeight = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    buf = np.empty((frameCount, frameHeight, frameWidth, 3), np.dtype('uint8'))
+    fc = 0
+    ret = True
+    while (fc < frameCount  and ret):
+        ret, buf[fc] = cap.read()
+        fc += 1
+    return buf
 
 
-(train_images, train_labels), (_, _) = tf.keras.datasets.mnist.load_data()
-train_images = train_images.reshape(train_images.shape[0], 28, 28, 1).astype('float32')
-train_images = (train_images - 127.5) / 127.5 # Normalize the images to [-1, 1], it was 0-255
-BUFFER_SIZE = 60000
+video_name = 'HarderBetterFasterStronger.mp4'
+video = getVideo(video_name)
+
+FRAMES = video.shape[0]
+IMG_HEIGHT = 360
+IMG_WIDTH = 480
+# print(FRAMES)
+video = video.reshape(FRAMES, IMG_HEIGHT, IMG_WIDTH, 3).astype('float32')
+video = (video - 127.5) / 127.5
+
+BUFFER_SIZE = FRAMES
 BATCH_SIZE = 256
 # Batch and shuffle the data
-train_dataset = tf.data.Dataset.from_tensor_slices(train_images).shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
+train_dataset = tf.data.Dataset.from_tensor_slices(video).shuffle(BUFFER_SIZE).batch(BATCH_SIZE, drop_remainder=True)
+# print(train_dataset)
 
 generator = make_generator_model()
 discriminator = make_discriminator_model()
+print(generator.summary())
+print(discriminator.summary())
+# noise = tf.random.normal([1, 100])
+# generated_image = generator(noise, training=False)
+#
+# print(type(generated_image))
+# print(generated_image.shape)
+# gen = generated_image[0, :, :, :] * 255
+# print(gen.shape)
+# plt.imshow((gen))
+# plt.show()
 
+#
 # This method returns a helper function to compute cross entropy loss
 cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
 
 generator_optimizer = tf.keras.optimizers.Adam(1e-4)
 discriminator_optimizer = tf.keras.optimizers.Adam(1e-4)
 
-checkpoint_dir = './training_checkpoints'
+checkpoint_dir = './training_checkpoints2'
 checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
 checkpoint = tf.train.Checkpoint(generator_optimizer=generator_optimizer,
                                  discriminator_optimizer=discriminator_optimizer,
                                  generator=generator,
                                  discriminator=discriminator)
 
-EPOCHS = 50
+EPOCHS = 16
 noise_dim = 100
 num_examples_to_generate = 16
 seed = tf.random.normal([num_examples_to_generate, noise_dim])
@@ -168,4 +199,3 @@ train(train_dataset, EPOCHS)
 # plt.imshow(generated_image[0, :, :, 0], cmap='gray')
 # plt.show()
 # input("Press Enter to continue...")
-# print(generator.summary())
